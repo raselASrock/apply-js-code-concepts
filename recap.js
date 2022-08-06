bdc49417b2f7ddb44d5672bce0b5b5dbee6042aa
raselASrock <mdraselm413@gmail.com>
2022-08-06 13:06:34 +0600

    recap-final

---
+++ recap.js
@@ -0,0 +1,38 @@
+// ********1. Variable*********
+
+var myName = "MD. Rasel Miah"
+
+// math operator: +, -, *, / ;
+// shorthand: +=, -=, *=, /= ;
+// Increment Decrement: ++, -- ;
+
+//***********2. Arrey*********
+
+var friends = ["abul", "tabul", "babul", "kabul", "jagul", "mongol", "tongol"];
+
+var thirdFriends = friends[2];
+friends[3] ="damboo";
+
+// ************3. Conditionals***********
+// >, <, ==, >=, <=, != ;
+// if(){} , else if(){},  else{};
+
+// **********4.Loop**********
+// while(){}
+// for(){}
+
+// 5.*******Function*********
+function isMoonUp(time){
+    if(time > 7){
+        return true;
+    }
+}
+
+
+// 6.*******Object*********
+var jantus  = {
+    height: 60,
+    romanticism: "heavvy",
+    bapErTaka: "borolok",
+    
+}
